dff16ae2adf5f1439bd65a70fda262a0b3b87903
dipaa183 <divaardanti183@gmail.com>
2025-06-03 20:39:19 +0700

    feat: membuat fitur register

--- Database/fotokopi_online.sql
+++ Database/fotokopi_online.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 03, 2025 at 01:59 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Jun 03, 2025 at 01:37 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,14 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `pemesanan` (
-  `id` int(11) NOT NULL,
-  `nama_pelanggan` varchar(100) DEFAULT NULL,
-  `file_upload` varchar(255) DEFAULT NULL,
-  `jenis_pemesanan` varchar(100) DEFAULT NULL,
-  `jenis_warna` varchar(50) DEFAULT NULL,
-  `ukuran_kertas` varchar(20) DEFAULT NULL,
-  `jumlah_copy` int(11) DEFAULT NULL,
-  `catatan` text DEFAULT NULL,
+  `id` int NOT NULL,
+  `nama_pelanggan` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `file_upload` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `jenis_pemesanan` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `jenis_warna` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `ukuran_kertas` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `jumlah_copy` int DEFAULT NULL,
+  `catatan` text COLLATE utf8mb4_general_ci,
   `tanggal_pemesanan` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -44,7 +44,28 @@ CREATE TABLE `pemesanan` (
 --
 
 INSERT INTO `pemesanan` (`id`, `nama_pelanggan`, `file_upload`, `jenis_pemesanan`, `jenis_warna`, `ukuran_kertas`, `jumlah_copy`, `catatan`, `tanggal_pemesanan`) VALUES
-(1, 'Diva ', '683eab4209a19_Templare Tugas Besar PAW.docx', 'Print,Jilid', 'Hitam Putih', 'A4', 1, 'Tolong dijilid dengan cover warna merah', '2025-06-03');
+(1, 'Diva Tes', '683eab4209a19_Templare Tugas Besar PAW.docx', 'Print,Jilid', 'Hitam Putih', 'A4', 1, 'Tolong dijilid dengan cover warna merah', '2025-06-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'Jordan Hodges', 'syro@mailinator.com', '$2y$10$.8eT02QioHbfamJ07D8RP.SF2p8GpYjFRTq2AmCZGuNAHPDSpV4Di', '2025-06-03 13:33:57');
 
 --
 -- Indexes for dumped tables
@@ -57,6 +78,13 @@ ALTER TABLE `pemesanan`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -64,7 +92,13 @@ ALTER TABLE `pemesanan`
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
