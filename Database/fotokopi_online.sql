2326f51deb0f5f8803b1b546ea655b59e65b8bf6
Wildannaufal10 <wildannaufal34@gmail.com>
2025-06-04 01:02:46 +0700

    wildan tambah admin

--- Database/fotokopi_online.sql
+++ Database/fotokopi_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 03, 2025 at 03:40 PM
+-- Generation Time: Jun 03, 2025 at 04:54 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.3.19
 
@@ -48,7 +48,8 @@ CREATE TABLE `pemesanan` (
 --
 
 INSERT INTO `pemesanan` (`id`, `user_id`, `nama_pelanggan`, `file_upload`, `jenis_pemesanan`, `jenis_warna`, `ukuran_kertas`, `jumlah_copy`, `catatan`, `tanggal_pemesanan`, `status`, `created_at`, `total_harga`) VALUES
-(1, NULL, 'Diva Tes', '683eab4209a19_Templare Tugas Besar PAW.docx', 'Print,Jilid', 'Hitam Putih', 'A4', 1, 'Tolong dijilid dengan cover warna merah', '2025-06-03', 'pending', '2025-06-03 15:02:12', NULL);
+(22, 4, 'Quidem ullam cupidat', '683f17d871ea0_cv_dump.pdf', 'Laminasi,Jilid', 'Hitam Putih', 'A4', 71, 'Molestias eius vero ', '2025-06-03', 'pending', '2025-06-03 15:42:17', 35500.00),
+(23, 6, 'Vel ut voluptate ut ', '683f1f9b31364__copy_Sample_document.docx', 'Print,Laminasi', 'Hitam Putih', 'Legal', 58, 'Dolore do et consequ', '2025-06-03', 'processing', '2025-06-03 16:15:24', 34800.00);
 
 -- --------------------------------------------------------
 
@@ -70,7 +71,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `role`) VALUES
-(1, 'Jordan Hodges', 'syro@mailinator.com', '$2y$10$.8eT02QioHbfamJ07D8RP.SF2p8GpYjFRTq2AmCZGuNAHPDSpV4Di', '2025-06-03 13:33:57', 'user');
+(1, 'Jordan Hodges', 'syro@mailinator.com', '$2y$10$.8eT02QioHbfamJ07D8RP.SF2p8GpYjFRTq2AmCZGuNAHPDSpV4Di', '2025-06-03 13:33:57', 'user'),
+(5, 'Administrator', 'admin@gmail.com', '$2y$10$40/l3a.vhSdtdAJaTMiWDelu76S.kec17mFtaLzkbzf1ZamltmBRa', '2025-06-03 16:10:17', 'admin'),
+(6, 'asep oli', 'asep@gmail.com', '$2y$10$dy.jf4YfTIJ3NB/HKd2sUueGf9F272ke7gCuUVHzUVA.koOR3YkT2', '2025-06-03 16:11:59', 'user');
 
 --
 -- Indexes for dumped tables
@@ -97,13 +100,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
